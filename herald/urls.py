7584d88b78073bed70ff5fce2ef50be38710dfa7
Sean Bermejo <sean.bermejo@gmail.com>
2020-05-10 20:48:41 +0800

    WIP add SendTestNotification

--- herald/urls.py
+++ herald/urls.py
@@ -4,9 +4,18 @@ Urls for herald app
 
 from django.conf.urls import url
 
-from .views import TestNotificationList, TestNotification
+from .views import SendTestNotification, TestNotification, TestNotificationList
 
 urlpatterns = [
-    url(r'^$', TestNotificationList.as_view(), name='herald_preview_list'),
-    url(r'^(?P<index>\d+)/(?P<type>[\w\-]+)/$', TestNotification.as_view(), name='herald_preview'),
+    url(r"^$", TestNotificationList.as_view(), name="herald_preview_list"),
+    url(
+        r"^(?P<index>\d+)/(?P<type>[\w\-]+)/$",
+        TestNotification.as_view(),
+        name="herald_preview",
+    ),
+    url(
+        r"^(?P<index>\d+)/(?P<type>[\w\-]+)/send/$",
+        SendTestNotification.as_view(),
+        name="send_test_notification",
+    ),
 ]
--- herald/views.py
+++ herald/views.py
@@ -3,6 +3,7 @@ Views for testing notifications. Should not be present in production
 """
 from django.conf import settings
 from django.http import HttpResponse
+from django.utils.html import escape
 from django.views.generic import TemplateView, View
 
 from . import registry
@@ -13,38 +14,52 @@ class TestNotificationList(TemplateView):
     View for listing out all notifications with links to view rendered versions of them
     """
 
-    template_name = 'herald/test/notification_list.html'
+    template_name = "herald/test/notification_list.html"
 
     def get_context_data(self, **kwargs):
         context = super(TestNotificationList, self).get_context_data(**kwargs)
 
-        context['notifications'] = [
-            (index, x.__name__, x.render_types, (y.__name__ for y in x.__bases__)) for index, x in enumerate(registry._registry)  # pylint: disable=W0212
+        context["notifications"] = [
+            (index, x.__name__, x.render_types, (y.__name__ for y in x.__bases__))
+            for index, x in enumerate(registry._registry)  # pylint: disable=W0212
         ]
 
         return context
 
 
-class TestNotification(View):
+class TestNotification(TemplateView):
     """
     View for showing rendered test notification
     """
 
-    def get(self, request, *args, **kwargs):  # pylint: disable=W0613
-        """
-        GET request
-        """
+    template_name = "herald/test/notification_detail.html"
 
-        index = int(kwargs['index'])
-        render_type = kwargs['type']
+    def get_notification(self):
+        index = int(self.kwargs["index"])
+        render_type = self.kwargs["type"]
 
-        obj = registry._registry[index](*registry._registry[index].get_demo_args())  # pylint: disable=W0212
+        return registry._registry[index](
+            *registry._registry[index].get_demo_args()
+        )  # pylint: disable=W0212
 
+    def get_content(self, obj):
         context = obj.get_context_data()
+        return obj.render(self.kwargs["type"], context)
 
-        content = obj.render(render_type, context)
+    def get_context_data(self, **kwargs):  # pylint: disable=W0613
+        context = super().get_context_data(**kwargs)
+        obj = self.get_notification()
+        kwargs["render_type"] = "plain" if render_type == "text" else render_type
+        kwargs["recipients"] = obj.get_recipients()
+        kwargs["content"] = escape(self.get_content(obj))
+        return kwargs
 
-        render_type = "plain" if render_type == "text" else render_type
-        charset = settings.DEFAULT_CHARSET
 
-        return HttpResponse(content, content_type='text/{}; charset={}'.format(render_type, charset))
+class SendTestNotification(TestNotification):
+    def post(self, request, *args, **kwargs):
+        try:
+            obj = self.get_notification()
+            obj.send()
+            return HttpResponse("Sent")
+        except KeyError:
+            return self.get(request, *args, **kwargs)
